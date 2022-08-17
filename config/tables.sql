892104c54e51092e4eeb23b675a4def15bce1c23
aldoBangun <punk.aldo68@gmail.com>
2022-08-17 10:17:22 +0800

    update tables.sql

--- config/tables.sql
+++ config/tables.sql
@@ -1,12 +1,6 @@
 CREATE TYPE genders AS ENUM('male', 'female');
 CREATE TYPE roles AS ENUM('admin', 'user');
 
-CREATE TABLE IF NOT EXISTS nationality(
-  nationality_id INT GENERATED ALWAYS AS IDENTITY,
-  nationality VARCHAR(30),
-  PRIMARY KEY(nationality_id)
-);
-
 CREATE TABLE IF NOT EXISTS users(
   user_id INT GENERATED ALWAYS AS IDENTITY,
   nationality_id INT,
@@ -17,10 +11,10 @@ CREATE TABLE IF NOT EXISTS users(
   user_image VARCHAR(255),
   gender genders,
   role roles,
+  nationality VARCHAR(50),
   created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
   updated_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY(user_id),
-  FOREIGN KEY(nationality_id) REFERENCES nationality(nationality_id)
 );
 
 CREATE TABLE IF NOT EXISTS destination(
@@ -32,30 +26,17 @@ CREATE TABLE IF NOT EXISTS destination(
   PRIMARY KEY(destination_id)
 );
 
-CREATE TABLE IF NOT EXISTS facilities(
-  facilities_id INT GENERATED ALWAYS AS IDENTITY,
-  PRIMARY KEY(facilities_id)
-);
-
-CREATE TABLE IF NOT EXISTS facilities_detail(
-  facilities_detail_id INT GENERATED ALWAYS AS IDENTITY,
-  facilities_id INT,
-  facility VARCHAR(50),
-  PRIMARY KEY(facilities_detail_id),
-  FOREIGN KEY(facilities_id) REFERENCES facilities(facilities_id)
-);
-
 CREATE TYPE airline_class AS ENUM('economy', 'business', 'first class');
 
 CREATE TABLE IF NOT EXISTS airlines(
   airline_id INT GENERATED ALWAYS AS IDENTITY,
-  facilities_id INT,
   airline_code VARCHAR(10),
   airline_name VARCHAR(100) NOT NULL,
   airline_image VARCHAR(255),
   class_category airline_class,
   price_adult INT DEFAULT 0,
   price_child INT DEFAULT 0,
+  facilities TEXT[],
   refundable BOOLEAN NOT NULL,
   reschedulable BOOLEAN,
   PRIMARY KEY(airline_id),
@@ -71,7 +52,7 @@ CREATE TABLE IF NOT EXISTS flight(
   FOREIGN KEY(destination_id) REFERENCES destination(destination_id)
 );
 
-CREATE TYPE status_ticket AS ENUM('success', 'waiting', 'failded');
+CREATE TYPE status_ticket AS ENUM('success', 'waiting', 'failed');
 
 CREATE TABLE IF NOT EXISTS booking(
   booking_id INT GENERATED ALWAYS AS IDENTITY,
@@ -81,6 +62,7 @@ CREATE TABLE IF NOT EXISTS booking(
   departure VARCHAR(50),
   flight_date TIMESTAMP WITH TIME ZONE,
   ticket_status status_ticket,
+  is_used boolean,
   PRIMARY KEY(booking_id),
   FOREIGN KEY(user_id) REFERENCES users(user_id),
   FOREIGN KEY(airline_id) REFERENCES airlines(airline_id)
