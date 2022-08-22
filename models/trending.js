9024b0bbb776ad9c20eeb95512ee268f666f8e5f
Ilham Yusuf <ilham.0992@students.amikom.ac.id>
2022-08-22 08:38:27 +0700

    [FIX] trending

--- models/trending.js
+++ models/trending.js
@@ -4,7 +4,7 @@ const ErrorResponse = require("../utils/ErrorResponse");
 const findTrending = () => {
 	return new Promise((resolve, reject) => {
 		db.query(
-			"SELECT booking.ticket_id, COUNT(*), destination.country, destination.city, destination.destination_image FROM booking LEFT JOIN tickets ON booking.ticket_id = tickets.ticket_id LEFT JOIN destination ON tickets.destination = destination.city GROUP BY booking.ticket_id ,destination.country, destination.city, destination.destination_image ORDER BY COUNT(*) DESC LIMIT 5",
+			"SELECT COUNT(*), destination.country, destination.city, destination.destination_image FROM booking LEFT JOIN tickets ON booking.ticket_id = tickets.ticket_id LEFT JOIN destination ON tickets.destination = destination.city GROUP BY destination.country, destination.city, destination.destination_image ORDER BY COUNT(*) DESC LIMIT 5",
 			(err, result) => {
 				if (err) return reject(err);
 				resolve(result);
@@ -16,7 +16,7 @@ const findTrending = () => {
 const findTenTrending = () => {
 	return new Promise((resolve, reject) => {
 		db.query(
-			"SELECT booking.ticket_id, COUNT(*), destination.country, destination.city, destination.destination_image FROM booking LEFT JOIN tickets ON booking.ticket_id = tickets.ticket_id LEFT JOIN destination ON tickets.destination = destination.city GROUP BY booking.ticket_id ,destination.country, destination.city, destination.destination_image ORDER BY COUNT(*) DESC LIMIT 10",
+			"SELECT COUNT(*), destination.country, destination.city, destination.destination_image FROM booking LEFT JOIN tickets ON booking.ticket_id = tickets.ticket_id LEFT JOIN destination ON tickets.destination = destination.city GROUP BY destination.country, destination.city, destination.destination_image ORDER BY COUNT(*) DESC LIMIT 10",
 			(err, result) => {
 				if (err) return reject(err);
 				resolve(result);
